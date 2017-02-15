b0e894409b25565366acb2eca2340b8426cd4fab
m1ksu <mikael.stenman112@gmail.com>
2017-02-15 13:59:12 +0200

    asdasdasdsad

--- script.js
+++ script.js
@@ -3,10 +3,49 @@ $(function () {
     var flipped = {'entresse': false};
 
     $(".shop").fadeOut();
+    $(".mall").fadeOut();
+
+
 
     $('#entresseico').click(function () {
-        $("#entresse").slideDown();
-        $("#entresse").css('visibility', 'visible');
+
+        open("entresse");
     });
 
+    $('#espoontoriico').click(function () {
+
+        open("espoontori");
+    });
+
+    $('.shop-view').click(function () {
+    	inside($(this).parent().parent().attr("id"));
+    });
+
+
+
+    $('.shop-close').click(function () {
+    	close($(this).parent().attr("id"));
+    });
+
+    function close (shop) {
+    	$("#" + shop).slideUp();
+    	setTimeout(function () {
+    		$("#" + shop + "ico").slideDown();
+    	}, 400);
+    }
+
+    function open (shop) {
+    	$("#" + shop).slideDown();
+        $("#" + shop).css('visibility', 'visible');
+        $("#" + shop + "ico").slideUp();
+    }
+
+    function inside (mall) {
+    	$("#" + mall + "mall, .mall").fadeIn();
+    	$("#" + mall + "mall, .mall").css("visibility", "visible");
+    }
+
+    function outside () {
+
+    }
 });
