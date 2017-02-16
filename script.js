fcbe06fbbf39580dd6e689450e7006504e6e777d
m1ksu <mikael.stenman112@gmail.com>
2017-02-16 14:09:21 +0200

    asdadsdasdas

--- script.js
+++ script.js
@@ -1,51 +1,73 @@
 $(function () {
 
-    $(".shop").fadeOut();
-    $(".mall").fadeOut();
+	$(".shop").fadeOut();
+	$(".mall-shop").fadeOut();
+	$(".mall").fadeOut();
 
 
-    $('#entresseico').click(function () {
-        open("entresse");
-    });
+	$('#entresseico').click(function () {
+		open("entresse");
+	});
 
-    $('#espoontoriico').click(function () {
-        open("espoontori");
-    });
+	$('#espoontoriico').click(function () {
+		open("espoontori");
+	});
+
+	// Entressen Kaupat
+		$('#spiceiceico').click(function () {
+			open("spiceice");
+		});
+	
+	// Espoontorin Kaupat
+		$('#kmarketico').click(function () {
+			open("kmarket");
+		});
+
+		$('#fitnessico').click(function () {
+			open("fitness");
+		});
+
+		$('#tokmanniico').click(function () {
+			open("tokmanni");
+		});
+
+		$('#pepeparturiico').click(function () {
+			open("pepeparturi");
+		});
+
+	$('.shop-view').click(function () {
+		inside($(this).parent().parent().attr("id"));
+	});
+
+	$('.mall-exit').click(function () {
+		outside();
+	});
+
+	$('.shop-close').click(function () {
+		close($(this).parent().attr("id"));
+	});
 
 
-    $('.shop-view').click(function () {
-    	inside($(this).parent().parent().attr("id"));
-    });
+	function close (shop) {
+		$("#" + shop).slideUp();
+		setTimeout(function () {
+			$("#" + shop + "ico").slideDown();
+		}, 400);
+	}
 
-    $('.mall-exit').click(function () {
-        outside();
-    });
+	function open (shop) {
+		$("#" + shop).slideDown();
+		$("#" + shop).css('visibility', 'visible');
+		$("#" + shop + "ico").slideUp();
+	}
 
-    $('.shop-close').click(function () {
-    	close($(this).parent().attr("id"));
-    });
+	function inside (mall) {
+		$("#" + mall + "mall").fadeIn();
+		$("#" + mall + "mall").css("visibility", "visible");
+	}
 
-
-    function close (shop) {
-    	$("#" + shop).slideUp();
-    	setTimeout(function () {
-    		$("#" + shop + "ico").slideDown();
-    	}, 400);
-    }
-
-    function open (shop) {
-    	$("#" + shop).slideDown();
-        $("#" + shop).css('visibility', 'visible');
-        $("#" + shop + "ico").slideUp();
-    }
-
-    function inside (mall) {
-    	$("#" + mall + "mall, .mall").fadeIn();
-    	$("#" + mall + "mall, .mall").css("visibility", "visible");
-    }
-
-    function outside () {
-        $('.mall').fadeOut();
-    }
+	function outside () {
+		$('.mall').fadeOut();
+	}
 
 });
